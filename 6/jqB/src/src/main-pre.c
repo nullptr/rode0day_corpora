08ed901a9afbcb544b389df6d6be36453cf53767
Josh Bundt <jmb@iseclab.org>
2020-02-13 20:46:14 +0000

    patches jq to remove commandline arguments

--- 6/jqB/src/src/main-pre.c
+++ 6/jqB/src/src/main-pre.c
@@ -4121,6 +4121,10 @@ jq_state *jq =
   int jq_flags = 0;
   size_t short_opts = 0;
   jv lib_search_paths = jv_null();
+  /* Rode0day -- force command line args */
+  options |= SLURP;
+  program = "[. | . | { hello: .  }]";
+  /* end - rode0day */
   for (int i=1; i<argc; i++, short_opts = 0) {
     if (args_done) {
       if (further_args_are_strings) {
