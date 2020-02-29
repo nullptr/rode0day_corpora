3ed5ca99de37e99332f7980248495821fbece61a
Andrew Fasano <fasano@mit.edu>
2020-02-29 15:00:33 -0500

    Another bugfix in singularity mode for eval_test. [skip ci]

--- scripts/eval_test.sh
+++ scripts/eval_test.sh
@@ -39,9 +39,9 @@ EOM
 SECONDS=0
 if [ "$RUNC" = "singularity" ]; then
     if [[ "$no_sbatch" ]]; then
-        echo "$TARGETS" | xargs -i{} -p 7 sh -c "nohup ./scripts/launch_job.sh ${fuzzer} '{}' 2 --test ${no_sbatch} > testing-singularity-${fuzzer}-{}.log"
+        echo "$TARGETS" | xargs -I{} -P 7 sh -c "nohup ./scripts/launch_job.sh ${fuzzer} '{}' 2 --test ${no_sbatch} > testing-singularity-${fuzzer}-{}.log"
     else
-        echo "$TARGETS" | xargs -i{} -p 2 sh -c "./scripts/launch_job.sh ${fuzzer} '{}' 2 --test "
+        echo "$TARGETS" | xargs -I{} -P 2 sh -c "./scripts/launch_job.sh ${fuzzer} '{}' 2 --test "
     fi
 else
     echo "$TARGETS" | xargs -I{} -P 7 sh -c "nohup ./scripts/launch.sh --test --fuzzer ${fuzzer} -N 2 '{}' > testing-docker-${fuzzer}-{}.log"
