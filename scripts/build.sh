b4b3da577a92a0dd34bee6e8dadc16a2f74d82d0
Josh Bundt <jmb@iseclab.org>
2020-04-09 00:07:13 +0000

    fixes time_limits for Eclipser and Ankou
    - Eclipser and Ankou both require a time limit argument on the comand
      line for the total fuzzing duration.  Previously, this was set to a
      default of 86400 (24hr), but wasn't easily configured.  The default is
      still 86400, but it should be adjusted in build.sh when required.  The
      experimentXX.sh scripts also have a default MAXTIME that can be
      adjusted if using those scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -88,11 +88,11 @@ create_job_files() {
         ./scripts/create-configs.py -e 3/jpegb/qsym_job.json -c qsym_job.json -p lava-gcc -Q -j QSYM -y ${i}/info.yaml $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/honggfuzz_job.json -c honggfuzz_job.json -p lava-gcc -Q -j HF -y ${i}/info.yaml $MERGE >/dev/null
 #       ./scripts/create-configs.py -e 3/jpegb/honggfuzz_job.json -c honggfuzz_job.json -p lava-hf -j HF -y ${i}/info.yaml -F $MERGE >/dev/null
-        ./scripts/create-configs.py -e 3/jpegb/eclipser_job.json -c eclipser_job.json -p lava-gcc -Q -j EC -y ${i}/info.yaml $MERGE >/dev/null
+        ./scripts/create-configs.py -e 3/jpegb/eclipser_job.json -c eclipser_job.json -p lava-gcc -Q -j EC -y ${i}/info.yaml --time-limit 86400 $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/angora_job.json -c angora_job.json -p lava-ang -j Ang -y ${i}/info.yaml $MERGE >/dev/null
 #       ./scripts/create-configs.py -e 3/jpegb/aflpp_job.json -c aflpp_job.json -p lava-afl-cf -j AFL++ -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
 #       ./scripts/create-configs.py -e 3/jpegb/aflrb_job.json -c aflrb_job.json -p lava-afl-cf -j AFLrb -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
-        ./scripts/create-configs.py -e 3/jpegb/ankou_job.json -c ankou_job.json -p lava-afl-cf -j AK -y ${i}/info.yaml $MERGE >/dev/null
+        ./scripts/create-configs.py -e 3/jpegb/ankou_job.json -c ankou_job.json -p lava-afl-cf -j AK -y ${i}/info.yaml --time-limit 86400 $MERGE >/dev/null
     done
     echo "[*] all job config files created!"
 }
--- scripts/experiment03.sh
+++ scripts/experiment03.sh
@@ -5,6 +5,9 @@
 
 [ -e scripts/build.sh ] || exit 1
 
+MAXTIME=$(( 60 * 60 * 24 * 1 ))
+
+
 if [[ $# -eq 0 ]]; then
     ./scripts/build.sh --download
     ./scripts/build.sh --copy-files
@@ -32,12 +35,12 @@ create_job_files() {
     for i in 0 {2..15}; do
         ./scripts/create-configs.py -e 3/jpegb/qsym_job.json -c qsym_job.json -p lava-gcc -Q -j QSYM -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/honggfuzz_job.json -c honggfuzz_job.json -p lava-gcc -Q -j HF -y ${i}/info.yaml -F $MERGE >/dev/null
-        ./scripts/create-configs.py -e 3/jpegb/eclipser_job.json -c eclipser_job.json -p lava-gcc -Q -j EC -y ${i}/info.yaml -F $MERGE >/dev/null
+        ./scripts/create-configs.py -e 3/jpegb/eclipser_job.json -c eclipser_job.json -p lava-gcc -Q -j EC -y ${i}/info.yaml -F --time-limit $MAXTIME $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/angora_job.json -c angora_job.json -p lava-ang -j Ang -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/afl_job.json -c afl_job.json -p lava-gcc -Q -j AFL -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/aflpp_job.json -c aflpp_job.json -p lava-afl-cf -j AFL++ -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
         ./scripts/create-configs.py -e 3/jpegb/aflrb_job.json -c aflrb_job.json -p lava-afl-cf -j AFLrb -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
-        ./scripts/create-configs.py -e 3/jpegb/ankou_job.json -c ankou_job.json -p lava-afl-cf -j AK -y ${i}/info.yaml -F -M 0 $MERGE >/dev/null
+        ./scripts/create-configs.py -e 3/jpegb/ankou_job.json -c ankou_job.json -p lava-afl-cf -j AK -y ${i}/info.yaml -F -M 0 --time-limit $MAXTIME $MERGE >/dev/null
     done
     echo "[*] all job config files created!"
 }
